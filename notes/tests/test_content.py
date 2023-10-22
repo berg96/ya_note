710bd95c040ff15691c7dfb88b289e62e4a33fca
Artem Kulikov <chigar2010@yandex.ru>
2023-10-22 20:57:31 +0200

    update

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -8,6 +8,7 @@ class TestContent(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='IamGroot')
+        cls.reader = User.objects.create(username='IceFrog')
         Note.objects.bulk_create(
             Note(
                 title=f'Note{index}',
@@ -17,6 +18,11 @@ class TestContent(TestCase):
             )
             for index in range(2)
         )
+        cls.the_note = Note.objects.create(
+            title='the_title',
+            text='the_text',
+            author=cls.author,
+        )
 
     def test_notes_count(self):
         self.client.force_login(self.author)
@@ -24,3 +30,28 @@ class TestContent(TestCase):
             len(self.client.get(reverse('notes:list')).context['object_list']),
             Note.objects.count()
         )
+
+    def test_notes_list_for_different_users(self):
+        users_availability = (
+            (self.author, True),
+            (self.reader, False)
+        )
+        for user, availability in users_availability:
+            self.client.force_login(user)
+            with self.subTest(user=user):
+                self.assertEqual(
+                    (self.the_note in self.client.get(
+                        reverse('notes:list')
+                    ).context['object_list']),
+                    availability
+                )
+
+    def test_pages_contains_form(self):
+        for name, args in (
+            ('notes:add', None),
+            ('notes:edit', (self.the_note.slug,))
+        ):
+            self.client.force_login(self.author)
+            self.assertIn(
+                'form', self.client.get(reverse(name, args=args)).context
+            )
